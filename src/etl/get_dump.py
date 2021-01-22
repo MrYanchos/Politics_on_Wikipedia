3105a0f0dff52dd3644f15baed9740fcbb4c6f53
cat028 <cat028@cat028-16398.pods.cat028.svc.cluster.local>
2021-01-21 20:55:50 -0800

    for downloading dump

---
+++ src/etl/get_dump.py
@@ -0,0 +1,8 @@
+import py7zr as pz
+import requests
+from bs4 import BeautifulSoup
+from src.libcode import list_to_txt, txt_to_list
+import io
+
+# see Cami1.ipynb for rough code
+# will transfer here later
