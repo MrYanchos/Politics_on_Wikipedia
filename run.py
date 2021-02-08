bccea74463b435f5efcabaee40c9efa96fdf59e7
jtdelval <jdelval@jdelval-2045.pods.jdelval.svc.cluster.local>
2021-02-07 18:23:13 -0800

    config and update run.py

--- run.py
+++ run.py
@@ -1,7 +1,9 @@
 import sys
 import src.etl.get_anames as gn
 import src.etl.get_atexts as gt
+import src.etl.get_ibc as gibc
 import src.models.get_gns_scores as gns
+import src.models.partyembed-ibc as pei
 
 args = sys.argv[1:]
 fname = ""
@@ -14,6 +16,18 @@ if "retrieve_anames" in args:
         artnames = gn.retrieve()
     else:
         artnames = gn.retrieve(fname)
+        
+if "ibc" in args:
+    with open('config/get_ibc_params.json') as fh:
+        data_cfg = json.load(fh)
+        
+    gibc.sample_ibc(**data_cfg)
+
+if "interpret_ibc" in args:
+    with open('config/interpret_ibc_params.json') as fh:
+        data_cfg = json.load(fh)
+    
+    pei.interpret_ibc(**data_cfg)
     
 if "all" in args:
     # fname = gn.scrape()
@@ -26,6 +40,10 @@ if "test" in args:
     nametxt_dict = gt.retrieve_atexts(test=True)
     namestat_dict = gns.get_stat_dict(nametxt_dict, test=True)
     print(namestat_dict)
+    gibc.sample_ibc("False")
+    print("Running model on test data...")
+    pei(temp_directory="test/temp", out_directory = 'test/out', agg_func='mean')
+    print("Finished, output in test/out/means.csv")
     
 
 
--- src/etl/get_ibc.py
+++ src/etl/get_ibc.py
@@ -2,12 +2,12 @@ import requests
 import tarfile
 import os
 
-def sample_ibc(already_obtained=False):
+def sample_ibc(already_obtained="False"):
     '''
     Downloads sample_ibc dataset.
     For instructions on how to download the full dataset, see README.md
     '''
-    if not already_obtained:
+    if already_obtained == "False":
         print('NOTICE: Sample IBC data is being downloaded.')
         print('Please see README.md for instructions on how to obtain the full dataset.')
         print('Once downloaded, please set the config to reflect that change.')
